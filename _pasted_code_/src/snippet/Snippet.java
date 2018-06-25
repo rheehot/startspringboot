6ad08b2494323a919f8ac9ef835f1a90b61e6384
LAPTOP-29RV9SMV\rheeh <rheehot@gmail.com>
2018-06-25 11:40:12 +0900

    boot 4 study (2)

---
+++ _pasted_code_/src/snippet/Snippet.java
@@ -0,0 +1,13 @@
+package snippet;
+
+public class Snippet {
+	public static void main(String[] args) {
+		import org.springframework.data.domain.Page;
+		import org.springframework.data.domain.Pageable;
+		import org.springframework.data.jpa.repository.Query;
+		import org.springframework.data.repository.CrudRepository;
+		import org.springframework.data.repository.query.Param;
+		import org.zerock.domain.Board;
+	}
+}
+
--- boot03/src/test/java/org/zerock/Boot03ApplicationTests.java
+++ boot03/src/test/java/org/zerock/Boot03ApplicationTests.java
@@ -8,10 +8,10 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-//import org.springframework.data.domain.Page;
-//import org.springframework.data.domain.PageRequest;
-//import org.springframework.data.domain.Pageable;
-//import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.zerock.domain.Board;
 import org.zerock.persistence.BoardRepository;
@@ -38,12 +38,12 @@ public class Boot03ApplicationTests {
 //
 //	}
 
-	@Test
-	public void testByTitle() {
-
-		repo.findBoardByTitle("Title!!!..117").forEach(board -> System.out.println(board));
-
-	}
+//	@Test
+//	public void testByTitle() {
+//
+//		repo.findBoardByTitle("Title!!!..117").forEach(board -> System.out.println(board));
+//
+//	}
 	
 //	Hibernate: select board0_.bno as bno1_0_, board0_.content as content2_0_, board0_.regdate as regdate3_0_, board0_.title as title4_0_, board0_.updatedate as updateda5_0_, board0_.writer as writer6_0_ from tbl_boards board0_ where board0_.title=?
 //			Board(bno=522, title=Title!!!..117, writer=user07, content=Contents!!! ....117 Fullup , regdate=2018-06-23 16:25:02.0, updatedate=2018-06-23 16:25:02.0)
@@ -65,6 +65,16 @@ public class Boot03ApplicationTests {
 //		results.forEach(board -> System.out.println(board));
 //	}
 //
+	
+//	@Test
+//	public void testByWriterContaining() {
+//
+//		Collection<Board> results = repo.findByTitleContainingOrContentContaining("05","Title");
+//
+//		results.forEach(board -> System.out.println(board));
+//
+//	}
+	
 //	@Test
 //	public void testByTitleAndBno() {
 //
@@ -73,11 +83,11 @@ public class Boot03ApplicationTests {
 //		results.forEach(board -> System.out.println(board));
 //
 //	}
-//
+
 //	@Test
 //	public void testBnoOrderBy() {
 //
-//		Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(90L);
+//		Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(400L);
 //		results.forEach(board -> System.out.println(board));
 //
 //	}
@@ -99,9 +109,10 @@ public class Boot03ApplicationTests {
 //	@Test
 //	public void testBnoPagingSort() {
 //
-//		Pageable paging = new PageRequest(0, 10, Sort.Direction.ASC, "bno");
+//	Pageable paging = PageRequest.of(0, 10, Sort.Direction.ASC, "bno");
 //
-//		Collection<Board> results = repo.findByBnoGreaterThan(0L, paging);
+//		
+//		Collection<Board> results = repo.findByBnoGreaterThanOrderByBnoDesc(0L, paging);
 //		results.forEach(board -> System.out.println(board));
 //
 //	}
@@ -145,7 +156,7 @@ public class Boot03ApplicationTests {
 //	@Test
 //	public void testByPaging() {
 //
-//		Pageable pageable = new PageRequest(0, 10);
+//		Pageable pageable = PageRequest.of(0, 10);
 //
 //		repo.findBypage(pageable).forEach(board -> System.out.println(board));
 //
